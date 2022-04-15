520f6962673cdc1357a921c2da4cca7f408cafbd
申思维 <shensiwei@sina.com>
2022-04-15 10:08:11 +0800

    增加了表单样式，分页，按钮样式

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,24 +3,31 @@ class BlogsController < ApplicationController
   before_action :get_by_id,:only =>[:show,:destroy,:edit,:update]
   layout 'a'
   def index
-    @blogs = Blog.all
+    @blogs = Blog
+    puts "--------------------hihih"
+    if params[:blog_title] != nil && params[:blog_title] != ''
+      @blogs = @blogs.where('name like ? ', "%#{params[:blog_title]}%")
+    end
+    if params[:blog_content] != nil && params[:blog_content] != ''
+      @blogs = @blogs.where('address like ? ', "%#{params[:blog_content]}%")
+    end
+    @blogs = @blogs.page(params[:page]).per(3)
     render :layout => 'a'
+    puts "-------end--index-----------hihih"
   end
 
   def new
     @blog = Blog.new
-    puts "=== in update, before : #{@blog.inspect}"
     @blog.title = params[:blog_title]
     @blog.time = params[:blog_time]
+    @blog.content = params[:blog_content]
     @blog.save!
-    puts "=== in update, after: #{@blog.inspect}"
     render :layout => 'b'
   end
 
   def create
     puts("==params is: ")
-    #Blog.create :time => params[:time], :title=> params[:title]
-    Blog.create(  { :time => params[:time], :title=> params[:title], :content => params[:content] }  )
+    blog.create(  { :time => params[:time], :title=> params[:title], :content => params[:content] }  )
     redirect_to blogs_path
   end
 
@@ -28,17 +35,10 @@ class BlogsController < ApplicationController
   end
 
   def update
-
-    #@blog = Blog.find(params[:id])
     puts "=== in update, before : #{@blog.inspect}"
-    @blog.title = params[:blog_title]
-    @blog.time = params[:blog_time]
-    @blog.content = params[:blog_content]
-    @blog.save!
-    puts "=== in update, after: #{@blog.inspect}"
-
     redirect_to blogs_path
-   #redirect_to :back
+  puts "=== in update, after: #{@blog.inspect}"
+
   end
 
   def show
