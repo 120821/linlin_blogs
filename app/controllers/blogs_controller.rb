2a4d547927a30575e3082ad0d9f6f1de3a2bca49
申思维 <shensiwei@sina.com>
2022-04-16 10:03:21 +0800

    修改了路由 增加了博客总数

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,15 @@
 class BlogsController < ApplicationController
 
   before_action :get_by_id,:only =>[:show,:destroy,:edit,:update]
-  layout 'a'
 
   def index
     @blogs = Blog
     puts "--------------------hihih"
     @blogs = @blogs.where('name like ? ', "%#{params[:blog_title]}%") if params[:blog_title].present?
     @blogs = @blogs.where('address like ? ', "%#{params[:blog_content]}%") if params[:blog_content].present?
+
+    @totul_count = @blogs.all.size
     @blogs = @blogs.page(params[:page]).per(30)
-    render :layout => 'a'
     puts "-------end--index-----------hihih"
   end
 
@@ -19,12 +19,11 @@ class BlogsController < ApplicationController
     @blog.time = params[:blog_time]
     @blog.content = params[:blog_content]
     @blog.save!
-    render :layout => 'b'
   end
 
   def create
     puts("==params is: ")
-    blog.create(  { :time => params[:time], :title=> params[:title], :content => params[:content] }  )
+    Blog.create(  { :time => params[:time], :title=> params[:title], :content => params[:content] }  )
     redirect_to blogs_path
   end
 
@@ -32,7 +31,6 @@ class BlogsController < ApplicationController
   end
 
   def update
-    #@blog = Blog.find(params[:id])
     puts "=== in update, before : #{@blog.inspect}"
     @blog.title = params[:blog_title]
     @blog.time = params[:blog_time]
@@ -40,11 +38,9 @@ class BlogsController < ApplicationController
     @blog.save!
     puts "=== in update, after: #{@blog.inspect}"
     redirect_to blogs_path
-    #redirect_to :back
   end
 
   def show
-    puts "==== im show"
   end
 
   def destroy
