567501bd168a8bcba6c7f748b05dc79c0813bc1e
申思维 <shensiwei@sina.com>
2022-04-26 14:20:05 +0800

    merge

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,11 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog
     puts "--------------------hihih"
-    @blogs = @blogs.where('name like ? ', "%#{params[:blog_title]}%") if params[:blog_title].present?
-    @blogs = @blogs.where('address like ? ', "%#{params[:blog_content]}%") if params[:blog_content].present?
+    @blog = @blog.where('name like ? ', "%#{params[:blog_title]}%") if params[:blog_title].present?
+    @blog = @blog.where('address like ? ', "%#{params[:blog_content]}%") if params[:blog_content].present?
+   # @blog = @blog.page(params[:page]).per(30)
 
-    @totul_count = @blogs.all.size
-    @blogs = @blogs.page(params[:page]).per(30)
+   # @total_count = @blog.all.size
     puts "-------end--index-----------hihih"
   end
 
@@ -19,11 +19,12 @@ class BlogsController < ApplicationController
     @blog.time = params[:blog_time]
     @blog.content = params[:blog_content]
     @blog.save!
+
   end
 
   def create
     puts("==params is: ")
-    Blog.create(  { :time => params[:time], :title=> params[:title], :content => params[:content] }  )
+    blog.create(  { :time => params[:time], :title=> params[:title], :content => params[:content] }  )
     redirect_to blogs_path
   end
 
@@ -31,6 +32,7 @@ class BlogsController < ApplicationController
   end
 
   def update
+    #@blog = Blog.find(params[:id])
     puts "=== in update, before : #{@blog.inspect}"
     @blog.title = params[:blog_title]
     @blog.time = params[:blog_time]
@@ -38,9 +40,11 @@ class BlogsController < ApplicationController
     @blog.save!
     puts "=== in update, after: #{@blog.inspect}"
     redirect_to blogs_path
+
   end
 
   def show
+    puts "==== im show"
   end
 
   def destroy
